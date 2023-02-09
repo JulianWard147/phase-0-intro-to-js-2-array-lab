385bfe8fa4ed1b1c5973a38a2cd4ec18b2e0505a
Julian Ward <wardjulianm@gmail.com>
2023-02-09 14:03:41 -0700

    shift and unshift are poor ways to title a method

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(newcat){
+    cats.push(newcat)
+    console.log(cats)
+}
+
+function destructivelyPrependCat(newcat){
+    cats.unshift(newcat)
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function appendCat(newcat){
+    return[...cats, newcat]
+}
+
+function prependCat(newcat){
+    return[newcat, ...cats]
+}
+
+function removeFirstCat(){
+    return(cats.slice(1))
+}
+
+function removeLastCat(){
+    return(cats.slice(0, (cats.length -1)))
+}
